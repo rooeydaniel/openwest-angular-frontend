207449f91d6a9c6f48a4343d8c7cab53312cd08c
Daniel Stephenson <dan.stephenson@gmail.com>
2014-05-08 20:10:26 -0600

    Able to retrieve tasks from Django backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,21 @@
 'use strict';
 
-var taskApp = angular.module('TaskApp', ['ngRoute', 'TaskApp.controllers'])
-    .config(['$routeProvider', function($routeProvider) {
+var taskApp = angular.module('TaskApp', ['ngRoute', 'restangular', 'TaskApp.controllers'])
+    .config(['$routeProvider', 'RestangularProvider', function($routeProvider, RestangularProvider) {
         $routeProvider
             .when('/dashboard', {
                 templateUrl: 'partials/dashboard.tpl.html',
                 controller: 'DashboardController',
                 title: 'Dashboard'
             })
+            .when('/tasks', {
+                templateUrl: 'partials/tasks.tpl.html',
+                controller: 'TasksController',
+                title: 'Tasks'
+            })
             .otherwise({
                 redirectTo: '/dashboard'
             });
+
+        RestangularProvider.setBaseUrl('http://localhost:8001');
     }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('TaskApp.controllers', [])
-    .controller('DashboardController', ['$scope', function($scope) {
-        // empty for now
+    .controller('DashboardController', ['$scope', '$location', function($scope, $location) {
+        $scope.viewTasks = function() {
+            $location.path('/tasks');
+        }
+    }])
+    .controller('TasksController', ['$scope', 'Restangular', function($scope, Restangular) {
+        $scope.tasks = {};
+
+        $scope.tasksLoaded = false;
+        Restangular.one('tasks').getList()
+            .then(function(tasks) {
+                console.log('Tasks loaded...');
+                $scope.tasks = tasks;
+
+                $scope.tasksLoaded = true;
+            });
     }]);
